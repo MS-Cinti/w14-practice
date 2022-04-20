4af005e670df637fbb556f7d405e2cfaec0cd75e
MS-Cinti <szabadicintia@gmail.com>
2022-04-20 17:36:25 +0200

    added some comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 //ez történik a háztető alatt, amikor a jsx-ben létrehozunk egy divet pl
-function Header(props) {
+function Header({to}) {
     //ha a returnnel egy sorban van a kód, akkor nem kell a () jele
   /*return <div>Header {toHeader}</div>*/ 
   //1.arg: milyen típusú elemet akarunk létrehozni, 2.arg: null, 3.arg: `` között szöveg
     //return React.createElement("div", null, `Header ${toHeader}`)
-    return <div>Header {props.to}</div>
+    //return <div>Header {to}</div> //input így néz ki: ({to})
+    return React.createElement("div", null, `Header ${to}`)
+
 }
 
 export default Header
